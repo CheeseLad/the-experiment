d00ffb3a0832dc8c42e8fa05345aab45c0fb5534
CheeseLad <cheesescreativeserver@gmail.com>
2023-10-29 01:47:38 +0100

    fixed main and added gitignore

--- main.py
+++ main.py
@@ -1,44 +1,85 @@
-import interactions
+#!/usr/bin/env python3
+
+import os
 import random
+import discord
+import requests
+import json
+import sqlite3
 from config import token
-from interactions import slash_command, SlashContext, slash_option, OptionType
 
-@slash_command(name="randomgif", description="Sends a random gif from The Experiment's curated collection.")
-async def my_command_function(ctx: SlashContext):
-    with open("gifs.txt", "r") as f:
-        gifs = f.readlines()
-    await ctx.send(random.choice(gifs).strip())
-
-@slash_command(name="addgif", description="Input the GIF you want to add to The Experiment's curated collection.")
-@slash_option(
-    name="string_option",
-    description="GIF URL",
-    required=True,
-    opt_type=OptionType.STRING,
-)
-async def my_command_function(ctx: SlashContext, string_option: str):
-    with open("gifs.txt", "a") as f:
-        f.write(string_option + "\n")
-    await ctx.send(f"Your GIF has been successfully added to The Experiment's curated collection.")
-
-@slash_command(name="removegif", description="Input the GIF you want to remove from The Experiment's curated collection.")
-@slash_option(
-    name="string_option",
-    description="GIF URL",
-    required=True,
-    opt_type=OptionType.STRING,
-)
-async def my_command_function(ctx: SlashContext, string_option: str):
-    with open("gifs.txt", "a") as f:
-        if string_option in f.read():
-            f.write(string_option + "\n")
-    await ctx.send(f"Your GIF has been successfully removed from The Experiment's curated collection.")
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        member_count = sum(1 for _ in client.get_all_members())
+        guild_count = sum(1 for _ in client.guilds)
+        await client.change_presence(activity=discord.Game(name=f'with {member_count} users in {guild_count} servers!'))
+    async def on_message(self, message):
+        
+        if message.author.id == self.user.id:
+            return
+        
+        if  message.content.startswith('!help'):
+            print(f"{message.author} used '!help'")
+            help_list = ["The Experiment - Commands List\n",
+                        "`!randomgif` - Sends a random gif from The Experiment's curated collection.", 
+                        "`!addgif` - Adds a GIF to The Experiment's curated collection.", 
+                        "`!removegif` - Removes a GIF from The Experiment's curated collection.", 
+                        "`!help` - Lists all the commands you can use with The Experiment."]
+            reply = []
+            for item in help_list:
+                reply.append(item + '\n')
+            await message.reply(f"{''.join(reply)}", mention_author=True)
 
 
-bot = interactions.Client()
 
-@interactions.listen()
-async def on_startup():
-    print(f"Bot is ready!")
+        if  message.content.startswith('!randomgif'):
+            print(f"{message.author} used '!randomgif'")
+            gifs = []
+            with open('gifs.txt', 'r') as f:
+                for line in f:
+                  gifs.append(line.strip())
+            response = random.choice(gifs)
+            await message.reply(f'{response}', mention_author=True)
 
-bot.start(token)
+        if  message.content.startswith('!addgif'):
+            print(f"{message.author} used '!addgif'")
+            response = message.content.split()[1]
+            gifs = []
+            with open('gifs.txt', 'r') as f:
+                for line in f.readlines():
+                  gifs.append(line.strip())
+            if response in gifs:
+                await message.reply("That GIF has already been added to The Experiement's curated collection.", mention_author=True)
+            elif "http" in response and "@" not in response:
+                with open('gifs.txt', 'a') as f:
+                  f.write(response + '\n')
+                await message.reply("Added that GIF to The Experiement's curated collection successfully.", mention_author=True)
+            else:
+                await message.reply('Your GIF format was not valid.', mention_author=True)
+
+        if message.content.startswith('!removegif'):
+            print(f"{message.author} used '!removegif'")
+            response = message.content.split()[1]
+            gifs = []
+            with open('gifs.txt', 'r') as f:
+                for line in f.readlines():
+                  gifs.append(line.strip())
+            if response in gifs:
+                gifs.remove(response)
+                with open('gifs.txt', 'w') as f:
+                    for gif in gifs:
+                        f.write(gif + '\n')
+                await message.reply("Removed that GIF from to The Experiement's curated collection successfully.", mention_author=True)
+            else:
+                await message.reply("That GIF was not found in The Experiement's curated collection.", mention_author=True)
+                    
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+client = MyClient(intents=intents)
+client.run(token)
+
